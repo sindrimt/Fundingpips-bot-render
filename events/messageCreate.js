94838b04ef98a2b1cb57b62404d28d1ea49fa780
Replit user <>
2023-11-10 21:22:32 +0100

    new log

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -6,7 +6,7 @@ const axios = require("axios");
 async function query(data) {
     try {
         const response = await fetch(
-            "https://www.stack-inference.com/run_deployed_flow?flow_id=654d27cb08992ae93d05abd8&org=efb9c8f5-9810-4a3a-bcc5-b6ab1335b664",
+            "https://www.stack-inference.com/run_deployed_flow?flow_id=6536cc6913a98a3d6b424b81&org=efb9c8f5-9810-4a3a-bcc5-b6ab1335b664",
             {
                 headers: { Authorization: "Bearer c01b8c21-bdf1-46be-b539-87bccedc77a4", "Content-Type": "application/json" },
                 method: "POST",
@@ -88,7 +88,6 @@ module.exports = {
                 try {
                     query({
                         "in-0": `${messageWithoutMention}`,
-                        "url-0": `https://fundingpips.freshdesk.com/support/home`,
                     })
                         .then((response) => {
                             console.log(response["out-0"]);
